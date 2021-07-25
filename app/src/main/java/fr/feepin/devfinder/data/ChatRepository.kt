d9eb0889f1200c8f9df56c415eaf7a0b252f1d31
David DEKEUWER <david.dekeuwer@gmail.com>
2021-07-25 12:32:02 +0200

    Add ChatRepository interface

---
+++ app/src/main/java/fr/feepin/devfinder/data/ChatRepository.kt
@@ -0,0 +1,15 @@
+package fr.feepin.devfinder.data
+
+import androidx.paging.PagingSource
+import com.google.firebase.Timestamp
+import fr.feepin.devfinder.data.models.Chat
+import fr.feepin.devfinder.data.models.Message
+
+interface ChatRepository {
+    suspend fun fetchChatsForUserId(userId: String): List<Chat>
+    suspend fun fetchLastMessageFromChatId(chatId: String): Message
+    suspend fun fetchRecentMessages(chatId: String, searchFromTs: Timestamp)
+    fun listenToNewMessage(chatId: String): Message
+    suspend fun addMessage(chatId: String, message: Message)
+    suspend fun addChat(chat: Chat)
+}
