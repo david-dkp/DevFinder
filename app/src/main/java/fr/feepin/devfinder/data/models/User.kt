be0137a8560ffe55901a0685277e2f928cc8faa8
David DEKEUWER <david.dekeuwer@gmail.com>
2021-07-27 20:34:21 +0200

    Add creationTs field to User model

--- app/src/main/java/fr/feepin/devfinder/data/models/User.kt
+++ app/src/main/java/fr/feepin/devfinder/data/models/User.kt
@@ -2,6 +2,7 @@ package fr.feepin.devfinder.data.models
 
 import com.google.firebase.firestore.DocumentId
 import com.google.firebase.firestore.PropertyName
+import java.sql.Timestamp
 
 data class User(
     @DocumentId val id: String? = null,
@@ -9,5 +10,6 @@ data class User(
     @PropertyName("profile_picture_url") val profilePictureUrl: String,
     val technologies: List<String>,
     val username: String,
-    val bio: String
+    val bio: String,
+    @PropertyName("creation_ts") val creationTs: Timestamp
 )
