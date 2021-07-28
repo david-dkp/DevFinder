5125dca83fab43f853f5b3e987d7b3191bb109c7
David DEKEUWER <david.dekeuwer@gmail.com>
2021-07-28 17:15:06 +0200

    Implement AppChatRepository

---
+++ app/src/main/java/fr/feepin/devfinder/data/repos/AppChatRepository.kt
@@ -0,0 +1,55 @@
+package fr.feepin.devfinder.data.repos
+
+import com.google.firebase.firestore.FieldPath
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import fr.feepin.devfinder.data.models.Chat
+import fr.feepin.devfinder.data.models.Message
+import kotlinx.coroutines.tasks.await
+
+class AppChatRepository : ChatRepository {
+
+    private val firestore = Firebase.firestore
+    private val chatsCollectionRef = firestore.collection("chats")
+    private val usersCollectionRef = firestore.collection("users")
+
+    override suspend fun fetchChatsByIds(chatsIdList: List<String>): List<Chat> {
+        return chatsCollectionRef
+            .whereIn(FieldPath.documentId(), chatsIdList)
+            .get()
+            .await()
+            .toObjects(Chat::class.java)
+    }
+
+    override suspend fun fetchLastMessageFromChatId(chatId: String): Message? {
+        return chatsCollectionRef
+            .document(chatId)
+            .collection("messages")
+            .orderBy("send_time_ts", Query.Direction.DESCENDING)
+            .limit(1)
+            .get()
+            .await()
+            .toObjects(Message::class.java)
+            .firstOrNull()
+    }
+
+    override suspend fun addChat(chat: Chat) {
+        chatsCollectionRef.add(chat).await()
+    }
+
+    override fun getMessagesQuery(chatId: String): Query {
+        return chatsCollectionRef
+            .document(chatId)
+            .collection("messages")
+            .orderBy("send_time_ts", Query.Direction.DESCENDING)
+            .limit(50)
+    }
+
+    override suspend fun addMessage(chatId: String, message: Message) {
+        chatsCollectionRef
+            .document(chatId)
+            .collection("messages")
+            .add(message)
+    }
+}
--- app/src/main/java/fr/feepin/devfinder/data/repos/ChatRepository.kt
+++ app/src/main/java/fr/feepin/devfinder/data/repos/ChatRepository.kt
@@ -2,14 +2,15 @@ package fr.feepin.devfinder.data.repos
 
 import androidx.paging.PagingSource
 import com.google.firebase.Timestamp
+import com.google.firebase.firestore.Query
 import fr.feepin.devfinder.data.models.Chat
 import fr.feepin.devfinder.data.models.Message
 
 interface ChatRepository {
-    suspend fun fetchChatsForUserId(userId: String): List<Chat>
-    suspend fun fetchLastMessageFromChatId(chatId: String): Message
-    suspend fun fetchRecentMessages(chatId: String, searchFromTs: Timestamp): List<Message>
-    fun listenToNewMessage(chatId: String): Message
-    suspend fun addMessage(chatId: String, message: Message)
+    suspend fun fetchChatsByIds(chatsIdList: List<String>): List<Chat>
+    suspend fun fetchLastMessageFromChatId(chatId: String): Message?
     suspend fun addChat(chat: Chat)
+
+    fun getMessagesQuery(chatId: String): Query
+    suspend fun addMessage(chatId: String, message: Message)
 }
