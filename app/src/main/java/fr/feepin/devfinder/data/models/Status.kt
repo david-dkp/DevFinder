7fbee852cef9a0007f7908bba9f89aedae3c2802
David DEKEUWER <david.dekeuwer@gmail.com>
2021-08-02 09:59:00 +0200

    Add @PropertyName to Status

--- app/src/main/java/fr/feepin/devfinder/data/models/Status.kt
+++ app/src/main/java/fr/feepin/devfinder/data/models/Status.kt
@@ -2,9 +2,10 @@ package fr.feepin.devfinder.data.models
 
 import com.google.firebase.Timestamp
 import com.google.firebase.firestore.DocumentId
+import com.google.firebase.firestore.PropertyName
 
 data class Status(
     @DocumentId val id: String? = null,
     val online: Boolean,
-    val lastTimeOnlineTs: Timestamp
+    @PropertyName("last_time_online_ts") val lastTimeOnlineTs: Timestamp
 )
