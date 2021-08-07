2e287c505cd5985f87ab5a19c628501539074322
David DEKEUWER <david.dekeuwer@gmail.com>
2021-08-07 11:41:04 +0200

    Update Level, add textResId to enum and public static method to retrieve the level from a number

--- app/src/main/java/fr/feepin/devfinder/data/models/Level.kt
+++ app/src/main/java/fr/feepin/devfinder/data/models/Level.kt
@@ -1,7 +1,22 @@
 package fr.feepin.devfinder.data.models
 
-enum class Level(val level: Int) {
-    BEGINNER(1),
-    INTERMEDIATE(2),
-    ADVANCED(3),
+import androidx.annotation.StringRes
+import fr.feepin.devfinder.R
+
+enum class Level(val level: Int, @StringRes val textResId: Int) {
+    BEGINNER(1, R.string.beginner_level_text),
+    INTERMEDIATE(2, R.string.intermediate_level_text),
+    ADVANCED(3, R.string.advanced_level_text);
+
+    companion object {
+        fun getLevelByNumber(number: Int): Level? {
+            return when(number) {
+                1 -> BEGINNER
+                2 -> INTERMEDIATE
+                3 -> ADVANCED
+                else -> null
+            }
+        }
+    }
+
 }
